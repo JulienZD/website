518859b9066d441164b85d8839d8c316cafbc104
Julien <jzapata@live.nl>
2021-01-10 17:41:59 +0100

    fix: increase links section animation delay

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 const julienLogo = document.querySelector('#julienLogo');
 
 julienLogo.addEventListener('animationstart', () => {
-	setTimeout(() => $('#elsewhereContainer').css('animation', '1s appear forwards'), 900);
+	setTimeout(() => $('#elsewhereContainer').css('animation', '1s appear forwards'), 1500);
 });
