085d216ee26bd32ca247737e8190520647a390bb
Julien <jzapata@live.nl>
2021-02-14 15:44:16 +0100

    feat: add copy to clipboard button

--- components/Colors/ColorCard.jsx
+++ components/Colors/ColorCard.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import styles from './ColorCard.module.css';
 
 function OutlinedButton({ color }) {
   return (
-    <button className="btn btn-sm mr-1" style={{ color: color, border: `1px solid ${color}` }}>
+    <button className="btn btn-sm mr-1" style={{ color: color, border: `1px solid ${color}` }} tabIndex="-1">
       Button
     </button>
   );
@@ -18,7 +18,6 @@ function ColoredCircle({ color }) {
           height: 15px;
           width: 15px;
           display: inline-block;
-          vertical-align: middle;
         }
       `}</style>
     </>
@@ -27,31 +26,52 @@ function ColoredCircle({ color }) {
 
 const ColorDescriptor = ({ text, color }) => {
   return (
-    <small>
-      <span>
-        {text}: {color.toLowerCase()}
-      </span>
+    <small className="d-flex flex-nowrap align-items-center">
+      <span className="text-nowrap">{`${text}: ${color.toLowerCase()}`}</span>
       <ColoredCircle color={color} />
     </small>
   );
 };
 
 function CardFooter({ primary, secondary, onClick }) {
+  const [copyBtn, setCopyBtnText] = useState({ icon: 'clipboard', title: 'Copy to clipboard' });
+  const toClipboard = async () => {
+    if (copyBtn.icon !== 'clipboard') return;
+    await navigator.clipboard.writeText(`background-color: ${primary};\ncolor: ${secondary};`);
+    setCopyBtnText({ icon: 'check2', title: 'Copied!' });
+  };
+
+  useEffect(() => {
+    if (copyBtn.icon === 'clipboard') return;
+    const timeout = setTimeout(() => setCopyBtnText({ icon: 'clipboard', title: 'Copy to clipboard' }), 2000);
+    return () => clearTimeout(timeout);
+  }, [copyBtn]);
+
   return (
     <div className={`${styles.cardFooter} row no-gutters justify-content-between bg-dark text-light`}>
-      <div className="d-flex flex-column">
+      <div className="col d-flex flex-column">
         <ColorDescriptor text="Primary" color={primary} />
         <ColorDescriptor text="Secondary" color={secondary} />
       </div>
-      <button className={`btn-sm button--gold ${styles.button}`} onClick={onClick}>
-        Invert
-      </button>
+      <div className="col d-flex justify-content-end" style={{ color: 'var(--main-text)' }}>
+        <button
+          title={copyBtn.title}
+          className={`btn-sm btn-link btn ${styles.button}`}
+          onClick={toClipboard}
+          aria-label={copyBtn.title}
+        >
+          <i className={`bi bi-${copyBtn.icon}`}></i>
+        </button>
+        <button className={`btn btn-sm btn-link ${styles.button}`} onClick={onClick} aria-label="Invert colors">
+          <i className="bi bi-circle-half"></i>
+        </button>
+      </div>
     </div>
   );
 }
 
 export default function ColorCard({ primary, secondary }) {
-  const [theme, setTheme] = React.useState({ primary: primary, secondary: secondary });
+  const [theme, setTheme] = useState({ primary: primary, secondary: secondary });
 
   const colorStyle = (background, foreground) => ({ backgroundColor: background, color: foreground });
   const getStyle = (alt) =>
@@ -69,7 +89,7 @@ export default function ColorCard({ primary, secondary }) {
         <p>The quick brown fox jumps over the lazy dog</p>
         <div className="d-flex mt-1">
           <OutlinedButton color={theme.secondary} />
-          <button className="btn btn-sm mr-1" style={getStyle(true)}>
+          <button className="btn btn-sm mr-1" style={getStyle(true)} tabIndex="-1">
             Button
           </button>
         </div>
