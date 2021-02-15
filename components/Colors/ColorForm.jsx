a4c4b19cfa3d3f28f9742494dcea77c859e79400
Julien <jzapata@live.nl>
2021-02-15 10:05:55 +0100

    refactor: lift form state up to previewer
    remove redundant color set state

--- components/Colors/ColorForm.jsx
+++ components/Colors/ColorForm.jsx
@@ -1,60 +1,35 @@
-import React, { Component } from 'react';
-
-export default class ColorForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      colors: props.defaultColors.join('\n'),
-      doShuffle: false,
-      hasClicked: false,
-    };
-    this.handleInputChange = this.handleInputChange.bind(this);
-  }
-
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
-    this.setState({ [name]: value }, this.onChangeCallback);
-  }
-
-  onChangeCallback() {
-    this.props.onChangeCallback(this.state);
-  }
-
-  handleSubmit = (event) => event.preventDefault();
-
-  render() {
-    return (
-      <form className="mx-auto" onSubmit={this.handleSubmit} style={{ maxWidth: '500px' }}>
-        <div className="form-group">
-          <label className="control-label" htmlFor="colorInput">
-            Colors (newline separated)
-          </label>
-          <textarea
-            id="colorInput"
-            name="colors"
-            className="form-control"
-            value={this.state.colors}
-            onChange={this.handleInputChange}
-            rows="10"
-            cols="5"
-          ></textarea>
-        </div>
-        <div className="custom-control custom-checkbox">
-          <input
-            id="shuffle"
-            name="doShuffle"
-            checked={this.state.doShuffle}
-            onChange={this.handleInputChange}
-            type="checkbox"
-            className="custom-control-input"
-          />
-          <label htmlFor="shuffle" className="custom-control-label">
-            Shuffle output
-          </label>
-        </div>
-      </form>
-    );
-  }
+export default function ColorForm({ colors, doShuffle, onColorChange, onDoShuffleChange }) {
+  const handleColorTextChange = (e) => onColorChange(e.target.value);
+  const handleDoShuffleChange = (e) => onDoShuffleChange(e.target.checked);
+  return (
+    <form className="mx-auto" onSubmit={(e) => e.preventDefault()} style={{ maxWidth: '500px' }}>
+      <div className="form-group">
+        <label className="control-label" htmlFor="colorInput">
+          Colors (newline separated)
+        </label>
+        <textarea
+          id="colorInput"
+          name="colors"
+          className="form-control"
+          value={colors}
+          onChange={handleColorTextChange}
+          rows="10"
+          cols="5"
+        ></textarea>
+      </div>
+      <div className="custom-control custom-checkbox">
+        <input
+          id="shuffle"
+          name="doShuffle"
+          checked={doShuffle}
+          onChange={handleDoShuffleChange}
+          type="checkbox"
+          className="custom-control-input"
+        />
+        <label htmlFor="shuffle" className="custom-control-label">
+          Shuffle output
+        </label>
+      </div>
+    </form>
+  );
 }
--- components/Colors/Previewer.jsx
+++ components/Colors/Previewer.jsx
@@ -2,42 +2,24 @@ import { useState } from 'react';
 import ColorForm from './ColorForm';
 import ColorDeck from './ColorDeck';
 
-function foo(input, currentSet) {
-  const colors = input
-    .trim()
-    .split('\n')
-    .filter((c) => c.match(/^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/));
-  const newSet = new Set(colors);
-  // Prevent epilepsy
-  if (areSetsEqual(newSet, currentSet) || newSet.size === input.length) return;
-  return newSet;
-}
-
 export default function ColorPreviewer({ initialColors }) {
-  const [doShuffle, shouldShuffle] = useState(false);
-  const [colorSet, setColorSet] = useState(new Set(initialColors));
+  const [colors, setColors] = useState(initialColors.join('\n'));
+  const [doShuffle, setDoShuffle] = useState(false);
 
-  const updateState = ({ colors, doShuffle }) => {
-    shouldShuffle(doShuffle);
-    setColorSet((current) => {
-      const newSet = foo(colors, current);
-      if (newSet) return newSet;
-      return current;
-    });
-  };
+  const getHexColorSet = (input) =>
+    new Set(
+      input
+        .trim()
+        .split('\n')
+        .filter((c) => c.match(/^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/))
+    );
 
   return (
     <div className="container">
       <div className="row">
-        <ColorForm onChangeCallback={updateState} defaultColors={Array.from(colorSet)} />
+        <ColorForm colors={colors} doShuffle={doShuffle} onColorChange={setColors} onDoShuffleChange={setDoShuffle} />
       </div>
-      <ColorDeck doShuffle={doShuffle} colors={colorSet}></ColorDeck>
+      <ColorDeck doShuffle={doShuffle} colors={getHexColorSet(colors)}></ColorDeck>
     </div>
   );
-}
-
-function areSetsEqual(setA, setB) {
-  if (setA.size !== setB.size) return false;
-  for (const a of setA) if (!setB.has(a)) return false;
-  return true;
 }
