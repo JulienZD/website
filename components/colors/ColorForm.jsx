458e0bc7bc07baf513241de1498674238019409c
Julien <jzapata@live.nl>
2021-04-05 13:22:32 +0200

    feat: add color picker to color previewer

--- components/colors/ColorForm.jsx
+++ components/colors/ColorForm.jsx
@@ -1,10 +1,15 @@
 import LabeledCheckbox from '@components/form/LabeledCheckbox';
+import ColorInput from '@components/form/ColorInput';
 
 export default function ColorForm({ colors, doShuffle, onColorChange, onDoShuffleChange }) {
   const handleColorTextChange = (e) => onColorChange(e.target.value);
   const handleDoShuffleChange = (e) => onDoShuffleChange(e.target.checked);
+  const handleColorInputChange = (newColor) => {
+    if (!colors.includes(newColor)) onColorChange(colors.concat(`\n${newColor}`));
+  };
   return (
     <form className="form w-48 mt-3" onSubmit={(e) => e.preventDefault()}>
+      <ColorInput onClose={handleColorInputChange} />
       <div className="form-group">
         <label className="mb-1" htmlFor="colorInput">
           Colors (newline separated)
