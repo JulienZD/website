cc22d58a7071b8e1c63cbd5bc56e43e932da3e7b
Julien <jzapata@live.nl>
2021-04-10 15:23:28 +0200

    refactor: extract intro component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,20 @@
-import { RefObject, useEffect, useRef } from 'react';
+import { createRef, RefObject, useEffect, useRef } from 'react';
 import Layout from '@components/layout';
 import EduProgress from '@components/EduProgress';
 import SocialButton from '@components/SocialButton';
+import AnimatedIntro from '@components/AnimatedIntro';
 
 interface Elements {
-  logo: RefObject<HTMLElement>;
-  elsewhereContainer: RefObject<HTMLElement>;
+  introRef: RefObject<HTMLElement>;
+  socialsContainer: RefObject<HTMLElement>;
   progressContainer: RefObject<HTMLElement>;
-  h1Ref: RefObject<HTMLElement>;
 }
 
 function displayIntro(elements: Elements) {
-  animateIntroText(elements.logo, elements.h1Ref);
   setAnimEvents(elements);
 }
 
-function animateIntroText(logo: RefObject<HTMLElement>, textRef: RefObject<HTMLElement>) {
-  if (!textRef.current || !logo.current) return;
-
-  const textNodes = Array.from(textRef.current.children) as HTMLElement[];
-
-  textNodes.forEach((node, index) => {
-    node.style.animationDuration = `${index + 1}s`;
-    node.style.animationDelay = `${index / 2}s`;
-  });
-
-  logo.current.style.animationDelay = '1.25s'; //`${textNodes.length}s`;
-}
-
-function setAnimEvents({ logo, elsewhereContainer, progressContainer }: Omit<Elements, 'h1Ref'>) {
+function setAnimEvents({ introRef, socialsContainer, progressContainer }: Elements) {
   const animateElementAfterPrevious = (
     { current: prevAnimatedEl }: RefObject<HTMLElement>,
     { current: nextAnimatedEl }: RefObject<HTMLElement>,
@@ -46,40 +32,25 @@ function setAnimEvents({ logo, elsewhereContainer, progressContainer }: Omit<Ele
     el.style.animationName = 'appear';
   };
 
-  animateElementAfterPrevious(logo, elsewhereContainer, 750);
-  animateElementAfterPrevious(elsewhereContainer, progressContainer, 0);
+  animateElementAfterPrevious(introRef, socialsContainer, 750);
+  animateElementAfterPrevious(socialsContainer, progressContainer, 0);
 }
 
 const endDate = new Date(2023, 5, 1);
 
 export default function Home() {
-  const logo = useRef<HTMLImageElement>(null);
-  const elsewhereContainer = useRef<HTMLDivElement>(null);
+  const socialsContainer = useRef<HTMLDivElement>(null);
   const progressContainer = useRef<HTMLDivElement>(null);
-  const h1Ref = useRef<HTMLHeadingElement>(null);
+  const introRef = createRef<HTMLImageElement>();
 
   useEffect(() => {
-    displayIntro({ logo, elsewhereContainer, progressContainer, h1Ref });
+    displayIntro({ introRef, socialsContainer: socialsContainer, progressContainer });
   });
   return (
     <Layout home>
       <div id="wrapper" className="text-left md:text-center">
-        <h1 className="sr-only">Hi, my name is Julien</h1>
-        <section id="introContainer">
-          <h1 ref={h1Ref} aria-hidden="true">
-            <span className="text-7xl"> Hi, </span>
-            <span className="text-5xl"> my name is </span>
-          </h1>
-          <img
-            ref={logo}
-            className="inline"
-            id="logo"
-            src="images/julien.svg"
-            aria-hidden="true"
-            title="I drew this myself, can you believe it?"
-          />
-        </section>
-        <section ref={elsewhereContainer} id="elsewhereContainer" className="mt-4 mt-sm-12">
+        <AnimatedIntro ref={introRef} />
+        <section ref={socialsContainer} id="elsewhereContainer" className="mt-4 mt-sm-12">
           <h2 className="section-title">Find me elsewhere</h2>
           <div className="pt-4">
             <SocialButton href="https://github.com/JulienZD" icon="github" ariaLabel="My GitHub" />
