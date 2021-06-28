03e5766534cf49fc5904a40b7d39113a4c59b000
Julien <jzapata@live.nl>
2021-06-28 10:12:51 +0200

    feat: use optimized images on homepage where possible

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,18 @@
-export default function Hero({ src, altSrc }: { src: string; altSrc: string }): JSX.Element {
+export default function Hero(): JSX.Element {
   return (
     <>
       <div
         className={`hidden lg:block xl:w-[900px] lg:w-[600px] h-[650px] relative float-right xl:bg-center bg-left-center bg-cover bg-no-repeat animate-slideLeft`}
         style={{
-          backgroundImage: `url(${src})`,
+          backgroundImage: `url(images/hero.jpg)`,
           clipPath: 'polygon(calc(30% - 10vw) 0, 100% 0, 100% 100%, 45% 100%)',
         }}
       />
       <div className="container block lg:hidden">
-        <img className="animate-slideUp rounded-t-xl" src={altSrc ?? src} alt={''} height="360" />
+        <picture>
+          <source srcSet="/images/hero-og.webp" type="image/webp" />
+          <img className="animate-slideUp rounded-t-xl" src="/images/hero-og.jpg" alt="" height={360} loading="lazy" />
+        </picture>
       </div>
     </>
   );
